21ad5084f5264938be7f7470a0bbeff10148dfc1
NIna Vitvinskaja <nvitvinskaja@mail.ru>
2021-04-23 17:16:18 +0300

    2спринт работает проект

---
+++ view/allnews.php
@@ -0,0 +1,56 @@
+allnews.php
+
+<?php
+ob_start();
+?>
+
+<h1>Kõik uudised</h1>
+<br>
+
+<?php
+ViewNews::AllNews($arr);
+$content = ob_get_clean();
+include_once 'view/layout.php';
+?>
+
+category.php
+
+<?php
+echo "<li class='submenuunit'><a href='all'>ALL</a></li><br>";
+foreach ($arr as $value) {
+echo "<li class='submenuunit'>
+
+<a href='category?id=".$value['id']."'>".$value['name'].'</a>
+</li>
+<br>';
+}
+
+?>
+
+catnews.php
+
+<?php
+ob_start();
+?>
+<h1>Uudised (kategoorii)</h1>
+<br>
+
+<?php
+ViewNews::NewsByCategory($arr);
+$content = ob_get_clean();
+include_once 'view/layout.php';
+
+?>
+
+error404.php
+
+<?php
+ob_start();
+?>
+
+<h1>Error404</h1>
+<?php
+$content = ob_get_clean();
+include_once 'view/layout.php';
+
+  ?>
